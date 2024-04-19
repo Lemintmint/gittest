73c7eba23377d48bf361834eace2ccdae82de6f9
Lemintmint <lemgm1237@gmail.com>
2024-04-19 19:15:27 +0900

    win commit and push test

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Happy Birthday, $name!",
         modifier = modifier
     )
 }
